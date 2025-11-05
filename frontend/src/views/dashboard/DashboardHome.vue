fc312bc7acfb53872efe625a7fe1eb963ce59e85
Yoann SALARD <administrateur@ergoland.fr>
2025-11-05 22:16:42 +0100

    fix: ajout de gardes v-if pour empêcher le crash du dashboard

--- frontend/src/views/dashboard/DashboardHome.vue
+++ frontend/src/views/dashboard/DashboardHome.vue
@@ -1,6 +1,6 @@
 <!-- frontend/src/views/dashboard/DashboardHome.vue -->
 <template>
-  <div>
+  <div v-if="user">
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
       <h1 class="h2"><font-awesome-icon icon="fa-solid fa-tachometer-alt" /> Tableau de Bord</h1>
     </div>
